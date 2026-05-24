a71b8606fb1584b8b7517959581846cca029eef3
satin <satin3737@gmail.com>
2026-05-24 11:51:52 +0300

    Added react plugins for the eslint, small changes for the reusable styles exports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,7 @@
 import js from '@eslint/js';
 import type {Linter} from 'eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import {defineConfig, globalIgnores} from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -36,8 +38,13 @@ export default defineConfig([
     globalIgnores(['dist']),
     {
         files: ['**/*.{ts,tsx}'],
-        extends: [js.configs.recommended, tseslint.configs.recommended],
-        languageOptions: {ecmaVersion: 2020, globals: globals.browser},
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite
+        ],
+        languageOptions: {globals: globals.browser},
         rules: customEsLintSettings
     }
 ]);
