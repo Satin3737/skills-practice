e17c19a4e8d691ea28c71df25fd5acb46efda9b0
satin <satin3737@gmail.com>
2026-06-23 21:34:26 +0300

    Added app entry point, added plugins autoload structure

---
+++ practice/fastify-practice/src/app.ts
@@ -0,0 +1,25 @@
+import autoload from '@fastify/autoload';
+import Fastify from 'fastify';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const srcDir = path.dirname(fileURLToPath(import.meta.url));
+
+const fastify = Fastify({
+    logger: true,
+    ajv: {customOptions: {allErrors: true}}
+});
+
+await fastify.register(autoload, {dir: path.join(srcDir, 'plugins')});
+await fastify.register(autoload, {dir: path.join(srcDir, 'modules')});
+
+const startServer = async () => {
+    try {
+        await fastify.listen({port: Number(process.env.PORT) || 3000});
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+void startServer();
